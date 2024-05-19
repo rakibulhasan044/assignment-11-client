4ef8e422c8ba5b2693dd2b8b3dcfa4ede0306ff9
rakibulhasan044 <rhrakib044@gmail.com>
2024-05-19 20:27:13 +0600

    design featured room

--- src/pages/home/FeaturedRooms.jsx
+++ src/pages/home/FeaturedRooms.jsx
@@ -1,27 +1,63 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/navigation";
+import { EffectCoverflow, FreeMode, Autoplay, Navigation } from "swiper/modules";
+import { Link } from "react-router-dom";
 
 const FeaturedRooms = () => {
-    const [rooms, setRooms] = useState([]);
+  const [rooms, setRooms] = useState([]);
 
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const {data} = await axios(`${import.meta.env.VITE_API_URL}/suite`);
-                setRooms(data)
-            }
-            catch (error) {
-                console.log(error);
-            }
-        }
-        getData();
-    },[])
-    console.log(rooms);
-    return (
-        <div>
-            
-        </div>
-    );
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios(`${import.meta.env.VITE_API_URL}/suite`);
+        setRooms(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+  console.log(rooms);
+
+  return (
+    <div className="mt-10 md:mt-16">
+      <Swiper
+        effect={"coverflow"}
+        slidesPerView={3}
+        spaceBetween={20}
+        grabCursor={true}
+        freeMode={true}
+        centeredSlides={true}
+        navigation={true}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        modules={[EffectCoverflow, FreeMode, Autoplay, Navigation]}
+        className="mySwiper"
+      >
+        {rooms.map((room) => (
+          <SwiperSlide key={room._id}>
+            <img src={room.images[0]} alt={room.title} />
+            <p className="py-2">{room.title}</p>
+            <p>{room.description.substring(0, 80)}...</p>
+            <Link to={`/room-details/${room._id}`} className="btn btn-outline btn-warning my-2 w-full">Book Now</Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default FeaturedRooms;
